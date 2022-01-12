6fcd8a49def666d44b3220d5e2924fa5b0cabd43
Беляев Сергей <sbelyaev@lan.lanit.ru>
2022-01-12 14:21:26 +0300

    Home work #1 added

---
+++ 01/HomeWork/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя");
+            string name = Console.ReadLine();
+            Thread.Sleep(5000);
+            Console.WriteLine($"Здарова, {name}!");
+            Thread.Sleep(5000);
+            Console.WriteLine($"Пошел ты нахуй {name}!");
+            Console.ReadKey();
+
+        }
+    }
+}
